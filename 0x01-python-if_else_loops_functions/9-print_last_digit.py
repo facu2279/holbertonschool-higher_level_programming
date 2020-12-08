e5280b2ebeea0ec419468be3602c4c852ed33d07
facu2279 <2279@holbertonschool.com>
2020-12-08 17:55:00 +0000

    add task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    negativo = 0
+    if number < 0:
+        number = number * -1
+        negativo = 1
     lastdigit = number % 10
+
+    if negativo == 1:
+        number = number * -1
+
     print('{:d}'.format(lastdigit), end="")
     return(lastdigit)
