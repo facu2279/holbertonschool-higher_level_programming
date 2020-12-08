f789ca10871f1ad3eda8b2a25cdcfef0186deed1
facu2279 <2279@holbertonschool.com>
2020-12-08 17:07:02 +0000

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,6 @@
 for i in range(9):
     for i2 in range(10):
         if i < i2:
-            print('{:d}{:d}, '.format(i, i2), end="")
+            if i < 8:
+                print('{:d}{:d}, '.format(i, i2), end="")
 print("89")
