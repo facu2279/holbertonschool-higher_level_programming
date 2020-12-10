46bad911e5bc77d5976b40f1e03ee5b00441a164
facu2279 <2279@holbertonschool.com>
2020-12-10 17:30:24 +0000

    add-101

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) == 4:
+        if sys.argv == "+"
+            print("".format())
+        elif:
+
+        elif:
+        
+        elif:
+
+        else:
+            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+            exit(1)
+
+    else:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit(1)
