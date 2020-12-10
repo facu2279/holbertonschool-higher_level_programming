4ade84fa27adb256b81f6d2662e90b2176af9319
facu2279 <2279@holbertonschool.com>
2020-12-10 16:23:25 +0000

    upload task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("{}".format("0 arguments."))
+elif len(sys.argv) == 2:
+    print("{}".format("1 argument:"))
+    print("{:d}{}{}".format(1, ": ", sys.argv[1]))
+else:
+    print("{:d} arguments".format(len(sys.argv) - 1))
+    i = 0
+    for i in range(len(sys.argv) - 1):
+        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
