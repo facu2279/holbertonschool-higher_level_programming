3822b7b9c2bb9a44d9ba32808d2eb6b26db8bb23
facu2279 <2279@holbertonschool.com>
2020-12-10 19:03:24 +0000

    edit pep8 task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,21 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        signo = sys.argv[2]
-        if signo == '+':
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif signo == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif signo == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif signo == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
-            exit(1)
-    else:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    else:
+        a = int(sys.argv[1])
+        i = sys.argv[2]
+        b = int(sys.argv[3])
+        if i == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif i == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif i == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        elif i == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
