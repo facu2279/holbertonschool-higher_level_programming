e6f906827675f7e4a01c5b4e124753567652613c
facu2279 <2279@holbertonschool.com>
2020-12-10 16:24:37 +0000

    upload task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("{}".format("0 arguments."))
-elif len(sys.argv) == 2:
-    print("{}".format("1 argument:"))
-    print("{:d}{}{}".format(1, ": ", sys.argv[1]))
-else:
-    print("{:d} arguments".format(len(sys.argv) - 1))
-    i = 0
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{}".format("0 arguments."))
+    elif len(sys.argv) == 2:
+        print("{}".format("1 argument:"))
+        print("{:d}{}{}".format(1, ": ", sys.argv[1]))
+    else:
+        print("{:d} arguments".format(len(sys.argv) - 1))
+        i = 0
+        for i in range(len(sys.argv) - 1):
+            print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
