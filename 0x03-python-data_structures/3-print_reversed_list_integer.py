2ccdce841a8f4c1512af4ea9a288b423d8dd8706
facu2279 <2279@holbertonschool.com>
2020-12-15 16:07:01 +0000

    fix task 3 again

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        tope = len(my_list)
+    if (my_list):
         my_list.reverse()
-        for i in range(0, tope - 1):
+        for i in range(0, len(my_list)):
             print("{:d}".format(my_list[i]))
-    else:
-        return
