050f6e0df4b2fcba3a919872e808e07bf20a8d14
facu2279 <2279@holbertonschool.com>
2020-12-15 22:05:49 +0000

    print size of the python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,13 @@
+#include <Python.h>
 /**
 * print_python_list_info - print information about lists in python
  * @p: A PyObject list
 */
 void print_python_list_info(PyObject *p)
 {
-	printf("[*] Size of the Python List = 2\n");
+    int tam;
+    
+    tam = Py_SIZE(p);
+	printf("[*] Size of the Python List = %d\n", tam);
 	printf("[*] Allocated = 2\n");
 }
