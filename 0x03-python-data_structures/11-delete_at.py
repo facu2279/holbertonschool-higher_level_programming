3c290df5d0755dc518faae8921a36a25875e6f17
facu2279 <2279@holbertonschool.com>
2020-12-15 20:44:17 +0000

    fix task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,6 +3,5 @@ def delete_at(my_list=[], idx=0):
         tope = len(my_list)
         if idx < tope and idx > 0:
             del my_list[idx]
-            return (my_list)
-        else:
-            return (my_list)
+        
+        return (my_list)
