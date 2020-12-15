ec34b6d9b53ce8ec9f703d605096abfc227a22a2
facu2279 <2279@holbertonschool.com>
2020-12-15 15:51:17 +0000

    fix task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if my_list:
         tope = len(my_list)
-        if idx > tope:
+        if idx >= tope:
             return
         elif idx < 0:
             return
