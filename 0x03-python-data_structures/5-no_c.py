b6a6654b81e5cf01fb4ca72efec2d4ac3cfd7d78
facu2279 <2279@holbertonschool.com>
2020-12-15 17:36:10 +0000

    add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    my_list = ""
     tope = len(my_string)
-    new_string = ""
-    j = 0
     for i in range(0, tope):
-        if my_string[i] != "c":
-            new_string[j] = my_string[i]
-        j = j + 1
-    
-    return (new_string)
+        if my_string[i] == "C" or my_string[i] == "c":
+            continue
+        my_list = my_list + my_string[i]
+    return(my_list)
