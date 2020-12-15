ab915788a8c9cf0a14ac236effd7d7ef36933266
facu2279 <2279@holbertonschool.com>
2020-12-15 21:58:26 +0000

    add file of advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,9 @@
+/**
+* print_python_list_info - print information about lists in python
+ * @p: A PyObject list
+*/
+void print_python_list_info(PyObject *p)
+{
+	printf("[*] Size of the Python List = \n");
+	printf("[*] Allocated = \n");
+}
