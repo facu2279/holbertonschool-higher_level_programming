1af571a451aad9aa8f18b529c80bc7ee92ba5f88
facu2279 <2279@holbertonschool.com>
2020-12-15 21:20:34 +0000

    fix task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        first_character = sentence[0]
-        largo_sentence = len(sentence)
-        return (largo_sentence, first_character)
-    else:
-        return (largo_sentence, None)
+        return len(sentence), sentence[0]
+    return len(sentence), None
