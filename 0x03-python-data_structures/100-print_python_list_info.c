24057498ac0c5f54bd1c680cb311fe9b968bfb0b
facu2279 <2279@holbertonschool.com>
2020-12-15 22:49:29 +0000

    finish

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,13 +14,12 @@ void print_python_list_info(PyObject *p)
     tope = Py_SIZE(p);
     cant = ((PyListObject *)p)->allocated;
 	printf("[*] Size of the Python List = %d\n[*] Allocated = %d\n", tope, cant);
-
     for (; i <= tope - 1; i++)
     {
         lista = PyList_GetItem((p), (i));
         printf("Element %d: %s\n", i, Py_TYPE(lista)->tp_name);
         /**
-        * tipo = Py_TYPE(lista)->tp_name;
+        * tipo = (Py_TYPE(lista)->tp_name);
 		*
         *printf("llego");
         */    
