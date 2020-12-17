fb83394cd64f1c1cb8e53a1da3523e6eb193edac
facu2279 <2279@holbertonschool.com>
2020-12-17 23:23:39 +0000

    test task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,9 +1,23 @@
+#include <Python.h>
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p)
 {
+	unsigned long int tope, i;
 
-
-
-    printf("[.] bytes object info");
-    printf("size: ");
-    printf("trying string: ");
+		tope = ((PyVarObject *)p)->ob_size;
+		printf("[*] Python list info\n");
+		printf("[*] Size of the Python List = %ld\n", tope);
+		printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+		for (i = 0; i < tope; i++)
+		{	
+			printf("Element %ld: %s\n", i, PySequence_GetItem(p, i)->ob_type->tp_name);
+		}
+}
+void print_python_bytes(PyObject *p)
+{
+(void)* p;
 }
