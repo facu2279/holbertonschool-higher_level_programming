267de4a7d7390c1092ede29e6bf6b97984b4b435
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-01-05 22:07:25 -0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Square class"""
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
