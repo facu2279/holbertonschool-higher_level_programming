0f52bca021a9c5ef4da5c6e0bd2467ea7fbedfff
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-01-05 22:09:41 -0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Square class"""
+    __size = None
+
+    def __init__(self, size=0):
+        """__init__"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >=0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
