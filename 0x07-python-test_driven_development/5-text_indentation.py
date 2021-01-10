502229094b90b847e9f06b47c42b2a2b844c9a27
facu2279 <2279@holbertonschool.com>
2021-01-10 18:21:07 +0000

    fix task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,3 +2,10 @@
 def text_indentation(text):
     if not type(text) is str:
         raise TypeError("text must be a string")
+    else:
+        text = text.replace(". ", ".@")
+        text = text.replace(": ", ":@")
+        text = text.replace("? ", "?@")
+        text = text.replace("@", "\n\n")
+
+        print("{}".format(text), end="")
