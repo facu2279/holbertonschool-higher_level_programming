110cee2a7ccfdafccb521e13414f5cd4bad42a1f
facu2279 <2279@holbertonschool.com>
2021-01-11 22:31:08 +0000

    Fix comments

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ set width """
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -42,7 +42,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Set height """
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
