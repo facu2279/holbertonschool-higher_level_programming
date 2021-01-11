11daabab01bd38cc18d2aff5a072c2bd581d005d
facu2279 <2279@holbertonschool.com>
2021-01-11 22:26:14 +0000

    Fix task and change the last if

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
