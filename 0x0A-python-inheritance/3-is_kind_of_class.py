d467bceedebe59ebe3326d30b7402e2ed8676afb
facu2279 <2279@holbertonschool.com>
2021-01-19 18:32:04 +0000

    Add task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ is kind of class function"""
+
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
