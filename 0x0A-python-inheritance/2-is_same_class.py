438076d3ca1b35fa9da62f4cbf919e03004ee390
facu2279 <2279@holbertonschool.com>
2021-01-19 18:30:19 +0000

    Add task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" is_same_class """
+
+
+def is_same_class(obj, a_class):
+    """ is the same class function"""
+    if type(obj) is a_class:
+        return (True)
+    else:
+        return (False)
