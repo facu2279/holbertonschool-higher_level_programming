d9bc3170c2a7dcc7c6d1284a4a778954b51c2712
facu2279 <2279@holbertonschool.com>
2021-01-20 16:21:29 +0000

    Fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    for line in open(filename):
-        print(line)
+    with open(filename) as file:
+        for line in file:
+            print(line)
