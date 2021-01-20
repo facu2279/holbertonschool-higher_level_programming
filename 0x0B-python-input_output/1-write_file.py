ffd22c015807109ef2742161a04c745f537bc7db
facu2279 <2279@holbertonschool.com>
2021-01-20 20:25:54 +0000

    Fix comments

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,5 @@ def write_file(filename="", text=""):
     """ class Student """
     cont = 0
     with open(filename, "w") as file:
-        for line in text:
-            cont = cont + len(line)
-        return (cont)
+        file.write(text)
+    return (len(text))
