4fe53f32af406059085ae3a3b7c0e354026b3903
facu2279 <2279@holbertonschool.com>
2021-01-20 20:24:05 +0000

    Change comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ class Student """
+
+
 def read_file(filename=""):
     """ class Student """
     with open(filename) as file:
         for line in file:
-            print(line)
+            print(line, end="")
