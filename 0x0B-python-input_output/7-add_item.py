91bae0f19bb59a3155a253fa58f02cc7fc1a3f05
facu2279 <2279@holbertonschool.com>
2021-01-20 17:45:31 +0000

    Add task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import json
+import os.path
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if (os.path.isfile("add_item.json")):
+    my_list = load_from_json_file("add_item.json")
+else:
+    my_list = []
+
+for i in range(1,len(sys.argv)):
+    my_list.append(sys.argv[i])
+
+save_to_json_file(my_list, "add_item.json")
