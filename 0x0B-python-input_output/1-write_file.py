3d85bf52358b8d241707cccf30ce49186887683a
facu2279 <2279@holbertonschool.com>
2021-01-20 14:20:38 +0000

    Add task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    cont = 0
+    with open(filename, "w") as file:
+        for line in text:
+            cont = cont + len(line)
+        return cont
