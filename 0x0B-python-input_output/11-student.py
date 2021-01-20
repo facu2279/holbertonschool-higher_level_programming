10d367aa3eac02a218d835bea6a99bf55e56e0ee
facu2279 <2279@holbertonschool.com>
2021-01-20 19:35:17 +0000

    Add task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" defines a class Student that defines a student
+    by: (based on 10-student.py) """
+
+
+class Student:
+    """ class Student """
+    def __init__(self, first_name, last_name, age):
+        """ def """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ def """
+        new = {}
+        if attrs == None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
+            return new
+    
+    def reload_from_json(self, json):
+        for i in json:
+            self.__dict__[i] = json[i]
