aaf7783be176f5a3b717f009c7d21e1182276aed
facu2279 <2279@holbertonschool.com>
2021-01-20 20:22:36 +0000

    Add comments

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" class Student """
+
+
 def write_file(filename="", text=""):
+    """ class Student """
     cont = 0
     with open(filename, "w") as file:
         for line in text:
