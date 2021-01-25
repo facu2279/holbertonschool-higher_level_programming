bce9827e34213bdcad9e00b67301bf664683fb3b
facu2279 <2279@holbertonschool.com>
2021-01-25 19:03:52 +0000

    Add task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ defines a class Rectangle """
 from models.base import Base
-
+import sys
 
 class Rectangle(Base):
     """ class Rectangle """
@@ -68,3 +68,21 @@ class Rectangle(Base):
         """ def """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
         self.__y, self.__width, self.__height)
+    
+    def update(self, *args):
+
+        if args is not None:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.intvalidator("width", args[1])
+                self.__width = args[1]
+            if len(args) > 2:
+                self.intvalidator("height", args[2])
+                self.__height = args[2]
+            if len(args) > 3:
+                self.intvalidator("x", args[3])
+                self.__x = args[3]
+            if len(args) > 4:
+                self.intvalidator("y", args[4])
+                self.__y = args[4]
