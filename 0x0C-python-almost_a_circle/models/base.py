fa39b41f2890a8900655286b1352fa8125beaa5c
facu2279 <2279@holbertonschool.com>
2021-01-25 17:14:12 +0000

    Add from 1 to 3

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" define a class Base """
+
+
+class Base:
+    """ class Base"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """define new object"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = self.__nb_objects + 1
+            self.id = self.__nb_objects
+    
+
+    def intvalidator(self, name, value):
+        """ this function check if value is a good type """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 and (name == "width" or name == "height"):
+            raise ValueError("{} must be > 0".format(name))
+        elif value < 0 and (name == "x" or name == "y"):
+            raise ValueError("{} must be >= 0".format(name))
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" defines a class Rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class Rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ def """
+        self.intvalidator("height", height)
+        self.__height = height
+        self.intvalidator("width", width)
+        self.__width = width
+        self.intvalidator("x", x)
+        self.__x = x
+        self.intvalidator("y", y)
+        self.__y = y
+        self.id = id
+        super(Rectangle, self).__init__(id)
+
+    @property
+    def width(self):
+        return self.width
+    @property
+    def height(self):
+        return self.height
+    @property
+    def x(self):
+        return self.x
+    @property
+    def y(self):
+        return self.y
+
+    @width.setter
+    def width(self, value):
+        self.intvalidator("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.intvalidator("height", value)
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.intvalidator("x", value)
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.intvalidator("y", value)
+        self.__y = value
