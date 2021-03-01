9a9235c9ca0b7acf368768a4106391f7370b4a3e
facu2279 <2279@holbertonschool.com>
2021-03-01 20:45:07 +0000

    Add task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- Create by Facundo Diaz to Holberton School 2021
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
-INSERT INTO second_table(id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)
