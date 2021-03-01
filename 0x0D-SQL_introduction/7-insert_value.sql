579338859c7b519a6101b44022c639b6c14287f8
facu2279 <2279@holbertonschool.com>
2021-03-01 19:22:01 +0000

    Fix task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Create by Facundo Diaz to Holberton School 2021
-INSERT INTO first_table ("id", "name") VALUES(89, "Holberton School");
+INSERT INTO 'first_table' ("id", "name") VALUES(89, "Holberton School");
