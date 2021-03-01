3e2b9afb2f863bbf967f4e6ce503bc76517cf9d9
facu2279 <2279@holbertonschool.com>
2021-03-01 22:29:25 +0000

    Fix task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Create by Facundo Diaz to Holberton School 2021
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
