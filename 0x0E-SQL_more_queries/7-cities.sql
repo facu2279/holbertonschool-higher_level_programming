19ea4ac79a3d11c761c709ff791bde15871944f3
facu2279 <2279@holbertonschool.com>
2021-03-02 19:31:31 +0000

    Fix task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- Create by Facundo Diaz to Holberton School 2021
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(PRIMARY KEY(id), id INT NOT NULL AUTO_INCREMENT, 
 state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id)
 );
