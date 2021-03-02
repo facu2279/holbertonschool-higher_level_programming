180432a250ef07eb545b8e3f11556396be296fc9
facu2279 <2279@holbertonschool.com>
2021-03-02 18:48:44 +0000

    Add task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Create by Facundo Diaz to Holberton School 2021
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INTEGER DEFAULT AUTO NOT NULL UNIQUE, name VARCHAR(256) NOT NULL)
