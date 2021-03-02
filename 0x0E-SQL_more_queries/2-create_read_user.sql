3636566c17c6042bab3c34a38df43c16e6a4cee3
facu2279 <2279@holbertonschool.com>
2021-03-02 18:16:23 +0000

    Fix task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create by Facundo Diaz to Holberton School 2021
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
