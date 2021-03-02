8db47148d1c7f200e356d3709a22839cb67c0976
facu2279 <2279@holbertonschool.com>
2021-03-02 18:35:16 +0000

    Add task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- Create by Facundo Diaz to Holberton School 2021
 
-CREATE TABLE IF NOT EXISTS unique_id (id INT(1) UNIQUE), name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS unique_id (id INT(1) NOT NULL UNIQUE), name VARCHAR(256))
