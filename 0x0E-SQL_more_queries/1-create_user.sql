a86cd3b775f87d3c459bd613b02088d7224297a1
facu2279 <2279@holbertonschool.com>
2021-03-02 17:27:37 +0000

    Add task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create by Facundo Diaz to Holberton School 2021
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON localhost.* TO 'user_0d_1'@'localhost';
