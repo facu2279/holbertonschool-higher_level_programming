9435e494b02d120b0077ae286df1485d95c0b019
facu2279 <2279@holbertonschool.com>
2021-03-03 01:05:10 +0000

    Add task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- Create by Facundo Diaz to Holberton School 2021
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
