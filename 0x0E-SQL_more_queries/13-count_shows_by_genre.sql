fe695355e5454d37bf22cdaa61067fc33a83bd86
facu2279 <2279@holbertonschool.com>
2021-03-03 01:24:53 +0000

    Add task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- Create by Facundo Diaz to Holberton School 2021
+
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY number_shows DESC;
