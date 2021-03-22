28bd25582a3f7015561604e9cf36ea72e0e543a2
facu2279 <2279@holbertonschool.com>
2021-03-22 18:27:09 +0000

    Fix task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 console.log(factorial(parseInt(argv[2])));
 
 function factorial (n) {
-  if (n == 0 || n == 1 || isNaN(n)) {
+  if (n === 0 || n === 1 || isNaN(n)) {
     return 1;
   } else {
     return n * factorial(n - 1);
