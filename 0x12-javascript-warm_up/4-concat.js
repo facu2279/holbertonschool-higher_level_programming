6acec49c47d5d5f8f87a2c10e4f1d1df87c107f0
facu2279 <2279@holbertonschool.com>
2021-03-22 17:01:37 +0000

    Add task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let i = 0;
+process.argv.forEach((val, index) => {
+  i++;
+});
+if (i < 3) {
+  console.log('undefined is undefined');
+}
+else
+{
+    console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
