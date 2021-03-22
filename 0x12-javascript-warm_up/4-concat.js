a31b92e1f4ebf9c411cbe87ff110539203c7712e
facu2279 <2279@holbertonschool.com>
2021-03-22 17:16:35 +0000

    Add task 4 semistandar checks

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-let val1 = 'undefined'; let val2 = 'undefined';
+let i = 0;
 process.argv.forEach((val, index) => {
-  if (index === 2) {
-    val1 = val;
-  }
-  if (index === 3) {
-    val2 = val;
-  }
+  i++;
 });
-console.log(val1 + ' is ' + val2);
+if (i < 3) {
+  console.log('undefined is undefined');
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
