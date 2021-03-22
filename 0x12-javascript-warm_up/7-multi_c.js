a846bc6d6ba0c6e6b30b4d73e25ccd9c7c844466
facu2279 <2279@holbertonschool.com>
2021-03-22 17:43:14 +0000

    Add task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const str = 'C is fun';
+if (isNaN(process.argv[2]))
+{
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i < process.argv[2]; i++) {
+    console.log(str);
+ }
+}
