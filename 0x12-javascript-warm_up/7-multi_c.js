c90445f7f63f57bc8c6f55591c05ffda95cb05f7
facu2279 <2279@holbertonschool.com>
2021-03-22 17:44:09 +0000

    Fix semistandar in task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const str = 'C is fun';
-if (isNaN(process.argv[2]))
-{
-    console.log('Missing number of occurrences');
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log(str);
- }
+  }
 }
