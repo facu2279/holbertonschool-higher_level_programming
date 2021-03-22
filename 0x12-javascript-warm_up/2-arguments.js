97c657a08559e260a5bf5c7da7a4d98c4c6b9c81
facu2279 <2279@holbertonschool.com>
2021-03-22 16:46:44 +0000

    Add task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length < 3)
+{
+    console.log('No argument');
+}
+if (process.argv.length > 3)
+{
+    console.log('Arguments found');
+}
+if (process.argv.length == 3)
+{
+    console.log('Argument found');
+}
