7651c725799ae54206cc537c95f14ee264d32678
facu2279 <2279@holbertonschool.com>
2021-03-23 17:50:35 +0000

    Add task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Made by Facundo Diaz to Holberton School 2021
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
