1b1a4e5aa45c0497ec4e49fabf74bb9a32bacadb
facu2279 <2279@holbertonschool.com>
2021-03-23 14:41:09 +0000

    Add task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Made by Facundo Diaz to Holberton School 2021
+class Rectangle {
+  constructor (h, w) {
+    this.width = h;
+    this.height = w;
+  }
+}
+module.exports = Rectangle;
