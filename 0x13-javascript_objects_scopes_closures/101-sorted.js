2b827eec606e077eb6d48cfdb904b43b6767c846
facu2279 <2279@holbertonschool.com>
2021-03-24 02:40:16 +0000

    Add task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Made by Facundo Diaz to Holberton School 2021
+
+const dict = require('./101-data').dict;
+const invertKeyValues = (dict, fn) => Object.keys(dict).reduce((acc, key) => {
+  const val = fn ? fn(dict[key]) : dict[key];
+  acc[val] = acc[val] || [];
+  acc[val].push(key);
+  return acc;
+}, {});
+console.log(invertKeyValues(dict));
