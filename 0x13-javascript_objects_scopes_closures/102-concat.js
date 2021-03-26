b567a414872bdc6ada2d7c44324c309e1a585cc2
facu2279 <2279@holbertonschool.com>
2021-03-26 15:02:22 +0000

    First commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-// xdxd
 const fs = require('fs');
 const first = process.argv[2];
 const second = process.argv[3];
 const path = process.argv[4];
-// xdxd
 let alltext = fs.readFileSync(first,
   { encoding: 'utf8', flag: 'r' },
   function (err, data) {
     if (err) { console.log(err); }
   });
-// xdxd
 alltext += fs.readFileSync(second,
   { encoding: 'utf8', flag: 'r' },
   function (err, data) {
     if (err) { console.log(err); }
   });
-// xdxd
 fs.writeFileSync(path, alltext);
