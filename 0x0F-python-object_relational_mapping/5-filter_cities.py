cf6f65009e9ab209592a9603aa1186c38b55a1d7
facu2279 <2279@holbertonschool.com>
2021-03-26 18:24:17 +0000

    Addt task 5 with pep8 checks

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Made by Facundo Diaz to Holberton School 2021 """
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+    MY_H = "localhost"
+    MY_U = argv[1]
+    MY_P = argv[2]
+    MY_D = argv[3]
+    argument = argv[4]
+    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaconexion.cursor()
+    texto1 = ""
+    texto2 = ""
+    consulta.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR\
+                     ', ') FROM cities INNER JOIN states ON states.name\
+                      = %s WHERE cities.state_id = states.id ORDER\
+                       BY cities.id ASC;", (argument,))
+    resultado = consulta.fetchall()
+    for fila in resultado:
+        proceso = str(fila).replace(",)", "")
+        proceso = proceso.replace("(", "")
+        proceso = proceso.replace("'", "")
+        print(proceso)
+    nuevaconexion.close()
