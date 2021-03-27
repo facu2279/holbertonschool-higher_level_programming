ce5531ca61ed3fe8c92e123c8c88ae8a30eeec60
facu2279 <2279@holbertonschool.com>
2021-03-27 12:25:50 +0000

    Fix print in task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     argument = argv[4]
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
-    consulta.execute("SELECT * FROM states WHERE name ='{}'".format(argument))
+    consulta.execute("SELECT * FROM states")
     resultado = consulta.fetchall()
     for fila in resultado:
-        print(fila)
+        if fila[1] == argument:
+            print(fila)
     nuevaconexion.close()
