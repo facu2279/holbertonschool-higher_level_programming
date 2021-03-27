26832df75a95d6b80bd966921b870573db9d31b9
facu2279 <2279@holbertonschool.com>
2021-03-27 12:38:39 +0000

    Fix error case in task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,8 +18,11 @@ if __name__ == "__main__":
                        BY cities.id ASC;", (argument,))
     resultado = consulta.fetchall()
     for fila in resultado:
-        proceso = str(fila).replace(",)", "")
-        proceso = proceso.replace("(", "")
-        proceso = proceso.replace("'", "")
-        print(proceso)
+            proceso = str(fila).replace(",)", "")
+            proceso = proceso.replace("(", "")
+            proceso = proceso.replace("'", "")
+            if (proceso != 'None'):
+                print(proceso)
+            else:
+                print()
     nuevaconexion.close()
