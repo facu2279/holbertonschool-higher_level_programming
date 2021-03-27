0df1ff4674e7f97c59abba376baaf0067650e0bb
facu2279 <2279@holbertonschool.com>
2021-03-27 15:11:40 +0000

    Fix times using commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,7 +7,6 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    new_id = 0
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
@@ -16,4 +15,4 @@ if __name__ == "__main__":
     for instance in session.query(State).order_by(State.id):
         if "a" in instance.name:
             session.delete(instance)
-            session.commit()
+    session.commit()
