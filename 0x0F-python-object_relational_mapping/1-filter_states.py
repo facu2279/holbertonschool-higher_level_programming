59b791e10ff17da1a0284d54114a48adaacd865b
facu2279 <2279@holbertonschool.com>
2021-03-27 12:15:46 +0000

    Fix print in task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     MY_D = argv[3]
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
-    consulta.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    consulta.execute("SELECT * FROM states ORDER BY id ASC")
     resultado = consulta.fetchall()
     for fila in resultado:
-        print(fila)
+        if fila[1][0] == 'N':
+            print(fila)
     nuevaconexion.close()
