6edae203a53ea2d6ea3972c6ac5ec62c1f1d00b6
facu2279 <2279@holbertonschool.com>
2021-03-27 14:50:01 +0000

    Fix opreation for calculate id

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -7,6 +7,7 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
+    new_id = 0
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
