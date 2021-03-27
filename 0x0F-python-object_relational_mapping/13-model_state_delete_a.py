f61ef078176d9a589365f8a6fff27c86ce36ba3a
facu2279 <2279@holbertonschool.com>
2021-03-27 15:06:33 +0000

    Add task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Made by Facundo Diaz for Holberton School 2021 """
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    new_id = 0
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if "a" in instance.name or "A" in instance.name:
+            session.delete(instance)
+            session.commit()
