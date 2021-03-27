fee3b4e4a3218f58b7b223dabe397e803817b6b9
facu2279 <2279@holbertonschool.com>
2021-03-27 12:10:53 +0000

    TEST

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N"""
-import MySQLdb
-import sys
+""" Made by Facundo Diaz to Holberton School 2021 """
 
+from sys import argv
+import MySQLdb
 if __name__ == "__main__":
-    MY_HOST = "localhost"
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
-    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS,
-                         db=MY_DB, port=3306, charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
-                ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    db.close()
-    cur.close()
+
+
+    MY_H = "localhost"
+    MY_U = argv[1]
+    MY_P = argv[2]
+    MY_D = argv[3]
+    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaconexion.cursor()
+    consulta.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    resultado = consulta.fetchall()
+    for fila in resultado:
+        if fila[1][0] == 'N':
+            print(fila)
+        """print(fila)"""
+    nuevaconexion.close()
