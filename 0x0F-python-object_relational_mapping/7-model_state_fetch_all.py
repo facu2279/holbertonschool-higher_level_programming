07d04a9aee8a398315a65700b7081433fdf9568b
facu2279 <2279@holbertonschool.com>
2021-03-27 13:12:35 +0000

    Add task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Made by Facundo Diaz to Holberton School 2021 """
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping = True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name)
+    for name in session.query(State.name).order_by(State.id):
+        print(name)
