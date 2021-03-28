940594861b6613622fd06b331f98227ab40e388d
facu2279 <2279@holbertonschool.com>
2021-03-28 14:07:52 +0000

    Add relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Made by Facundo Diaz to Holberton School 2021 """
 
+from relationship_city import Base, City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -13,5 +14,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", backref="state", cascade="all, delete")
