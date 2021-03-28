7103f98b50f080cf6f5141b2e3484dd8389fc679
facu2279 <2279@holbertonschool.com>
2021-03-28 14:11:07 +0000

    Remove nullable in class city

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Made by Facundo Diaz to Holberton School 2021 """
 
-from relationship_city import Base, City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -14,4 +13,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state")
