2da70cde0900f7616fac91928c15081e773ff278
facu2279 <2279@holbertonschool.com>
2021-04-10 02:41:51 +0000

    Fix link in task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
-if __name__ == '__main__':
-    xd = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(xd.json().get('id'))
+if __name__ == "__main__":
+    link = 'https://api.github.com/user'
+    linkxd = requests.get(link, auth=HTTPBasicAuth(argv[1], argv[2]))
+    if linkxd.status_code == 200:
+        dict = url.json()
+        print(dict['id'])
+    else:
+        print("None")
