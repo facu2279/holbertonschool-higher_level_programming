31b85c119989f450278fc4ed2c71b3fcab0b5e8a
facu2279 <2279@holbertonschool.com>
2021-04-10 02:46:06 +0000

    Fix authentication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """ Made by Facundo Diaz for Holberton School 2021 """
 import requests
-from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
-    link = 'https://api.github.com/user'
-    linkxd = requests.get(link, auth=HTTPBasicAuth(argv[1], argv[2]))
-    if linkxd.status_code == 200:
-        dict = url.json()
-        print(dict['id'])
-    else:
-        print("None")
+    userxd = sys.argv[1]
+    passxd = sys.argv[2]
+    link = "https://api.github.com/user"
+    res = requests.get(link, auth=(userxd, passxd))
+    print(res.json().get('id'))
