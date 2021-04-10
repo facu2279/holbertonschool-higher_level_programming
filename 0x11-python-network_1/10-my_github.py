19dbacfa7eea0faaa803ffd89f611cc15692e060
facu2279 <2279@holbertonschool.com>
2021-04-10 02:51:54 +0000

    Fix task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Made by Facundo Diaz for Holberton School 2021 """
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    userxd = sys.argv[1]
-    passxd = sys.argv[2]
-    link = "https://api.github.com/user"
-    res = requests.get(link, auth=(userxd, passxd))
-    print(res.json().get('id'))
+    user = sys.argv[1]
+    psw = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(user, psw))
+    print(response.json().get('id'))
