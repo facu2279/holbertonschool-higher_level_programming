654edfbb74e0d737914d5167a4650fd3693f12f7
facu2279 <2279@holbertonschool.com>
2021-04-10 00:43:19 +0000

    Fix 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Made by Facundo Diaz for Holberton School """
+""" Made by Facundo Diaz for Holberton School 2021 """
 import requests
 
-if __name__ == '__main__':
-    data = requests.get('https://intranet.hbtn.io/status')
-    res = data.text
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.content.decode('utf-8')))
