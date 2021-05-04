7cfd82b66a5891e1ca14d7e908242423bb43431c
facu2279 <2279@holbertonschool.com>
2021-05-04 23:46:48 +0000

    Fix task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,10 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    let completed = {};
-    let tasks = JSON.parse(body);
-    for (let i in tasks) {
-      let task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    }
-    console.log(completed);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log(data);
   }
 });
