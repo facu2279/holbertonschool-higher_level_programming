b1e002a86f0a534db3a5da40b24fe4c77d384ce1
facu2279 <2279@holbertonschool.com>
2021-05-05 00:08:26 +0000

    Fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+const link = process.argv[2];
+const nombre = process.argv[3];
+request(link, function (err, res, body) {
+  if (!err) {
+    fs.writeFile(filename, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+        console.error(err);
+    });
   }
 });
