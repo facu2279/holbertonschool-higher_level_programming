1d01f458b6ee687581de4d2cc23446b4fbfdab86
facu2279 <2279@holbertonschool.com>
2021-05-05 00:04:51 +0000

    Fixing semistandar

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
+const idpeli = process.argv[2];
+const res = 'https://swapi-api.hbtn.io/api/films/' + idpeli;
+request(res, function (err, res, body) {
+  if (!err) {
+    console.log(JSON.parse(body).title);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.logr(err);
   }
 });
