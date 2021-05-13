c15116a551622f9241f230a0a9d92b66c711d125
facu2279 <facu2279@holbertonschool.com>
2021-05-13 01:39:01 +0000

    Add task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+/* Made by Facundo Diaz for Holberton School 2021 */
+$(function () {
+    const xd = 'https://swapi-api.hbtn.io/api/films/?format=json';
+    $.get(xd, function (data, textStatus) {
+      const res = data.results;
+      for (const i in res) {
+        const item = $('<li></li>').text(res[i].title);
+        $('#list_movies').append(item);
+      }
+    });
+  });
